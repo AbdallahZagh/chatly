f6ce5a3b4145419e0ad106580c6ae3a79e1c3ef8
Karamkottish <ramosq16@gmail.com>
2026-01-06 21:27:01 +0300

    fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if($this->app->environment('production')) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         if (!defined('L5_SWAGGER_CONST_HOST')) {
             define('L5_SWAGGER_CONST_HOST', env('L5_SWAGGER_CONST_HOST', request()->root() . '/api'));
         }
